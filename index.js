41a02ef3ec441f07e147b9eae3b56d635b3dca11
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2019-10-03 16:34:41 +0100

    Added class

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.use(require('koa-static')('assets'))
 app.use(views(`${__dirname}`, { extension: 'html' }, {map: { handlebars: 'handlebars' }}))
 
 router.get('/', async ctx => await ctx.render('pages/index'))
+router.get('/login', async ctx => await ctx.render('pages/login'))
+router.get('/profile', async ctx => await ctx.render('pages/profile'))
+
 
 app.use(router.routes())
 module.exports = app.listen(port, () => console.log(`listening on port ${port}`))
