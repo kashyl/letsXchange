9a6390eda0bafe9ae2a031f1604217ae0874dc1b
kashyl <staviladeian@gmail.com>
2019-11-01 22:08:21 +0000

    Added ESLint for JS code analysis.
    Created gitignore and eslintignore files.
    
    Improved code using ESLint.

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 'use strict'
 
-//yeet
-
 const Koa = require('koa')
 const Router = require('koa-router')
 const app = new Koa()
@@ -13,16 +11,15 @@ const port = 8080
 
 app.use(require('koa-static')('.'))
 
-app.use(views(`${__dirname}`, { extension: 'html' }, {map: { handlebars: 'handlebars' }}))
+app.use(views(`${__dirname}`, { extension: 'html' }, { map: { handlebars: 'handlebars' } }))
 
-router.get('/', async ctx => await ctx.render('pages/index'))
-router.get('/login', async ctx => await ctx.render('pages/login'))
-router.get('/register', async ctx => await ctx.render('pages/register'))
-router.get('/profile', async ctx => await ctx.render('pages/profile'))
-router.get('/contact', async ctx => await ctx.render('pages/contact'))
-router.get('/listing', async ctx => await ctx.render('pages/listing'))
-router.get('/about', async ctx => await ctx.render('pages/about'))
-
+router.get('/', async ctx => ctx.render('pages/index'))
+router.get('/login', async ctx => ctx.render('pages/login'))
+router.get('/register', async ctx => ctx.render('pages/register'))
+router.get('/profile', async ctx => ctx.render('pages/profile'))
+router.get('/contact', async ctx => ctx.render('pages/contact'))
+router.get('/listing', async ctx => ctx.render('pages/listing'))
+router.get('/about', async ctx => ctx.render('pages/about'))
 
 app.use(router.routes())
 module.exports = app.listen(port, () => console.log(`listening on port ${port}`))
--- js/time-greeting.js
+++ js/time-greeting.js
@@ -1,13 +1,13 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
+var today = new Date()
+var hourNow = today.getHours()
+var greeting
 
 if (hourNow > 18) {
-    greeting = 'Good evening!';
+  greeting = 'Good evening!'
 } else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
+  greeting = 'Good afternoon!'
 } else if (hourNow > 0) {
-    greeting = 'Good morning!';
+  greeting = 'Good morning!'
 }
 
-document.write('<h3 class="time-greet">' + greeting + '</h3>');
+document.write('<h3 class="time-greet">' + greeting + '</h3>')
