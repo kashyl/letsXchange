515a469c003f4cce4c0cceed370d8a61f09611f3
kashyl <staviladeian@gmail.com>
2019-11-11 22:27:03 +0000

    Visitors can now register an account
    Register form fields now populate database.db users table
    
    if a users table is not found in the database, automatically make one
    
    Passwords are hashed (encrypted) using bcrypt

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ app.use(session(app))
 app.use(views(`${__dirname}`, { extension: 'handlebars' }, { map: { handlebars: 'handlebars' } }))
 
 const port = 8080
+/**
+ * For BCrypt - password hashing (encryption) function
+ * The higher the "salt rounds" number, the stronger the encryption
+ */
+const saltRounds = 10 
 const dbName = 'listings.db'
 
 router.get('/', async ctx => ctx.render('pages/index'))
@@ -60,9 +65,90 @@ router.get('/profile', async ctx => {
 router.get('/register', async ctx => {
     const data = {}
     if (ctx.query.msg) data.msg = ctx.query.msg
-    data.countries = ['UK', 'Europe', 'World']
-    await ctx.render('./pages/register.handlebars', data)
+    data.countries = ['UK', 'Europe', 'Other']
+    await ctx.render('./pages/register', data)
+})
+
+/**
+ * Script to process new user registration
+ * 
+ * @name Register Script
+ * @route {POST} /register
+ */
+router.post('/register', koaBody, async ctx => {
+    try {
+        const body = ctx.request.body
+        console.log(body)
+        // Processing file
+        const {path, type} = ctx.request.files.avatar
+        const fileExtension = mime.extension(type)
+        console.log(`path: ${path}`)
+        console.log(`type: ${type}`)
+        console.log(`fileExtension: ${fileExtension}`)
+        await fs.copy(path, 'assets/public/avatars/avatar.png')
+        // ENCRYPT PASSWORD, BUILD SQL
+        body.pass = await bcrypt.hash(body.pass, saltRounds)
+        const sql = `INSERT INTO users(user, pass) VALUES("${body.user}", "${body.pass}")`
+        console.log(sql)
+        // DATABASE COMMANDS
+        const db = await sqlite.open('./database/database.db')
+        await db.run(sql)
+        await db.close()
+        // Redirect user to home page
+        ctx.redirect(`/?msg=new user "${body.name}" added`)
+    } catch(err) {
+        await ctx.render('./pages/error', {message: err.message})
+    }
+})
+
+router.get('/login', async ctx => {
+    const data = {}
+    if (ctx.query.msg) data.msg = ctx.query.msg
+    if (ctx.query.user) data.user = ctx.query.user
+    await ctx.render('./pages/login', data)
+})
+
+router.post('/login', async ctx => {
+    try {
+        const body = ctx.request.body
+        const db = await sqlite.open('./database/database.db')
+        // Check if the username exists
+        const records = await db.get(`SELECT count(id) AS count FROM users WHERE user="${body.user}";`)
+        if (!records.count) return ctx.redirect('/login?msg=invalid%20username')
+        const record = await db.get(`SELECT pass FROM users WHERE user = "${body.user}";`)
+        await db.close()
+        // Check if the password matches
+        const valid = await bcrypt.compare(body.pass, record.pass)
+        if (valid == false) return ctx.redirect(`/login?user=${body.user}&msg=invalid%20password`)
+        // If the username and password are VALID
+        ctx.session.authorised = true // AUTHORISES SESSION
+        return ctx.redidrect('/msg=you are now logged in')
+    } catch(err) {
+        await ctx.render('./pages/error', {message: err.message})
+    }
+})
+
+// router.post('/login', async ctx => { // 19 lines reduced to 10!
+// 	const body = ctx.request.body
+// 	try {
+// 		await accounts.checkCredentials(body.user, body.pass)
+// 		ctx.session.authorised = true
+// 		return ctx.redirect('/?msg=you are now logged in...')
+// 	} catch(err) {
+// 		return ctx.redirect(`/login?user=${body.user}&msg=${err.message}`)
+// 	}
+// })
+
+router.get('/logout', async ctx => {
+    ctx.session.authorised = null;
+    ctx.redirect('/')
 })
 
 app.use(router.routes())
-module.exports = app.listen(port, () => console.log(`listening on port ${port}`))
+module.exports = app.listen(port, async() => {
+    // make sure the database has correct schema
+    const db = await sqlite.open('./database/database.db')
+    await db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, pass TEXT);')
+    await db.close()
+    console.log(`listening on port ${port}`)
+}) 
