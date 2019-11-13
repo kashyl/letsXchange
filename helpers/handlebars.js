e1741ebaf95694fed579083414f1f27ddda2024f
kashyl <staviladeian@gmail.com>
2019-11-13 22:32:05 +0000

    Added dependencies, created Handlebars helpers file, updated database, added misc files
    Updated package.json with new dependencies for npm install.
    
    Updated database with new fields for the users table.
    
    Created a Handlebars helpers (handlers) file with a test function, although not sure how to actually use it yet.

---
+++ helpers/handlebars.js
@@ -0,0 +1,5 @@
+// https://github.com/helpers/handlebars-helpers (list of many helpers)
+
+Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
