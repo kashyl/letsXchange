67169ef3a92ca12fadadb6b6ad62ab36cc7e89aa
kashyl <staviladeian@gmail.com>
2019-11-13 22:51:39 +0000

    Small fixes and changes, details in description.
    Time-greeting.js script output message slightly changed to work with personalized greeting on user log-in.
    
    Commented out handlebars conditional statement and script in header, as handlebars expression wasn't recognized by jQuery.load(). Todo: Find a way to load external files with Handlebars expression brackers ( {{//stuff}} ), as currently they're treated as text.
    
    Logout link on navigation bar will now turn to red on hover

--- js/time-greeting.js
+++ js/time-greeting.js
@@ -3,15 +3,13 @@ var hourNow = today.getHours()
 var greeting
 
 if (hourNow > 18) {
-  greeting = 'Good evening!'
+  greeting = 'Good evening'
 } else if (hourNow > 12) {
-  greeting = 'Good afternoon!'
+  greeting = 'Good afternoon'
 } else if (hourNow > 5) {
-  greeting = 'Good morning!'
+  greeting = 'Good morning'
 } else if (hourNow >= 0) {
-  greeting = 'Hello there, night owl!'
-} else {
-  greeting = 'Hello, what time was it again?'
+  greeting = 'Hello'
 }
 
 document.getElementById('time-greet').innerHTML = greeting
