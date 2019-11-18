5925dd9c92ba7277b8245e8ef155b0499e75af07
kashyl <staviladeian@gmail.com>
2019-11-18 17:55:32 +0000

    Select forms now populated with jQuery instead of HBS
    Using handlebars, select forms sometimes wouldn't populate before page loaded, resulting in no options. Switched to jQuery for reliability.

---
+++ js/populate-select.js
@@ -0,0 +1,17 @@
+const dropdown = $('select.loadjson')
+
+const name = dropdown.attr('name')
+
+dropdown.empty()
+
+dropdown.append('<option disabled selected> Choose an option </option>')
+dropdown.prop('selectedIndex', 0)
+
+const path = `../assets/json/${name}.json`
+
+// Populate dropdown with list from json
+$.getJSON(path, function (data) {
+    $.each(data, function (key, entry) {
+        dropdown.append($('<option></option>').attr('value', entry.name).text(entry.name))
+    })
+})
